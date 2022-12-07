bd12e4475bb65a398a711a6b73f906e77c29b1cb
Bar Nemesh <barnemesh@gmail.com>
2022-12-08 00:25:55 +0200

    some flat shading, spherical uv, and obj testing

---
+++ Assets/Mechanics/Displacement/From CG/MeshData.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MeshData
+{
+    public List<Vector3> vertices; // The vertices of the mesh 
+    public List<int> triangles; // Indices of vertices that make up the mesh faces
+    public Vector3[] normals; // The normals of the mesh, one per vertex
+
+    // Class initializer
+    public MeshData()
+    {
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
+    }
+
+    public void FromUnityMesh(Mesh mesh)
+    {
+        vertices = new List<Vector3>(mesh.vertices);
+        triangles = new List<int>(mesh.triangles);
+        normals = mesh.normals;
+    }
+
+    // Returns a Unity Mesh of this MeshData that can be rendered
+    public Mesh ToUnityMesh()
+    {
+        Mesh mesh = new Mesh
+        {
+            vertices = vertices.ToArray(),
+            triangles = triangles.ToArray(),
+            normals = normals
+        };
+
+        return mesh;
+    }
+
+    // Calculates surface normals for each vertex, according to face orientation
+    public void CalculateNormals()
+    {
+        normals = new Vector3[vertices.Count];
+        for (int i = 0; i < triangles.Count; i += 3)
+        {
+            Vector3 p1 = vertices[triangles[i]];
+            Vector3 p2 = vertices[triangles[i + 1]];
+            Vector3 p3 = vertices[triangles[i + 2]];
+
+            Vector3 normal = Vector3.Cross(p1 - p3, p2 - p3).normalized;
+
+            normals[triangles[i]] += normal;
+            normals[triangles[i + 1]] += normal;
+            normals[triangles[i + 2]] += normal;
+        }
+
+        for (int i = 0; i < normals.Length; i++)
+        {
+            normals[i] = normals[i].normalized;
+        }
+    }
+
+    // Edits mesh such that each face has a unique set of 3 vertices
+    public void MakeFlatShaded()
+    {
+        List<Vector3> newVertices = new List<Vector3>();
+        List<int> newTriangles = new List<int>();
+        for (int i = 0; i < triangles.Count; i += 3)
+        {
+            newVertices.Add(vertices[triangles[i]]);
+            newVertices.Add(vertices[triangles[i + 1]]);
+            newVertices.Add(vertices[triangles[i + 2]]);
+            newTriangles.Add(i);
+            newTriangles.Add(i + 1);
+            newTriangles.Add(i + 2);
+        }
+
+        triangles = newTriangles;
+        vertices = newVertices;
+    }
+}
