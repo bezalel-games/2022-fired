50b85a958f4b3bf9316a31f3246f093f424e9d0a
Gilad Binstok <giladbin@cs.huji.ac.il>
2022-12-08 12:55:22 +0200

    Update FireFactory.cs

--- Assets/Mechanics/Infection/FireFactory.cs
+++ Assets/Mechanics/Infection/FireFactory.cs
@@ -15,7 +15,7 @@ namespace Mechanics.Infection
         // Start is called before the first frame update
         void Start()
         {
-            
+            // hey
             _instance = this;
             _allFires = new LinkedPool<GameObject>(CreateFire, null, null, null, false, 200);
         }
