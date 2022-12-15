d4432ca5ebd555e9c8a8909f61ede75364216d31
Harel Don-Yehiya <harel.don@cs.huji.ac.il>
2022-12-15 09:43:33 +0200

    go

---
+++ Assets/Mechanics/NavMesh/PepoleAI/wallker.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using Pathfinding;
+
+[HelpURL("http://arongranberg.com/astar/documentation/stable/class_wandering_destination_setter.php")]
+public class wallker : MonoBehaviour {
+    public float radius = 20;
+
+    // IAstarAI ai;
+
+    // void Start () {
+    //     ai = GetComponent<IAstarAI>();
+    // }
+    //
+    // Vector3 PickRandomPoint () {
+    //     var point = Random.insideUnitSphere * radius;
+    //
+    //     point.y = 0;
+    //     point += ai.position;
+    //     return point;
+    // }
+    //
+    // void Update () {
+    //     // Update the destination of the AI if
+    //     // the AI is not already calculating a path and
+    //     // the ai has reached the end of the path or it has no path at all
+    //     if (!ai.pathPending && (ai.reachedEndOfPath || !ai.hasPath)) {
+    //         ai.destination = PickRandomPoint();
+    //         ai.SearchPath();
+    //     }
+    // }
+}
