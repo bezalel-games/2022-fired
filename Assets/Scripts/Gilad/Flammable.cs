c3ee8e821650ef13fe7821fac922ec0077cecc50
Bar Nemesh <barnemesh@gmail.com>
2022-12-28 21:02:56 +0200

    Flammable deactivates fires

--- Assets/Scripts/Gilad/Flammable.cs
+++ Assets/Scripts/Gilad/Flammable.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Flames;
 using UnityEngine;
 
 namespace Gilad
@@ -7,40 +8,59 @@ namespace Gilad
     public class Flammable : MonoBehaviour
     {
 
+        public static List<Flammable> AllFlammables => allFlammables;
+
         private static readonly List<Flammable> allFlammables = new List<Flammable>();
 
         private static int _numBurned = 0;
+        
         //flames to light
-        [SerializeField] private GameObject[] flames;
+        [SerializeField]
+        private GameObject[] flames;
 
         // how many its it takes to fully start the fire
-        [SerializeField] private int numOfHits = 1;
+        [SerializeField]
+        private int numOfHits = 1;
 
         // on what time do we do a auto grow
-        [SerializeField] private float hitDuration = 0.5f;
+        [SerializeField]
+        private float hitDuration = 0.5f;
 
         // how many hits does a fire ball worth
-        [SerializeField] private int firePower = 1;
+        [SerializeField]
+        private int firePower = 1;
 
         // how many hits does a water ball worth
-        [SerializeField] private int waterPower = 1;
+        [SerializeField]
+        private int waterPower = 1;
 
         //how many hits does time worth
-        [SerializeField] private int timePower = 1;
+        [SerializeField]
+        private int timePower = 1;
 
-        [SerializeField] private float lifeTime = 20f;
+        [SerializeField]
+        [Tooltip("After this time the object is considered burned")]
+        private float lifeTime = 20f;
+
+        [SerializeField]
+        private BurnedEvent onBurnedEvent;
 
         private float _timeOnFire = 0f;
-
-
-
-        public static List<Flammable> AllFlammables => allFlammables;
-
+        
         private int _powerLevel = 0;
 
         private Vector3[] _startSizes;
 
         private float _timeCount = 0f;
+
+        private void Awake()
+        {
+            if (onBurnedEvent != null)
+            {
+                onBurnedEvent = GetComponentInParent<BurnedEvent>();
+            }
+        }
+
         // Start is called before the first frame update
         void Start()
         {
@@ -49,11 +69,13 @@ namespace Gilad
             {
                 throw new Exception("num of hits must be larger than 0");
             }
+
             _startSizes = new Vector3[flames.Length];
             for (int i = 0; i < flames.Length; i++)
             {
                 _startSizes[i] = flames[i].transform.localScale / numOfHits;
                 flames[i].transform.localScale = Vector3.zero;
+                flames[i].SetActive(false);
             }
         }
 
@@ -68,13 +90,14 @@ namespace Gilad
                     ShutDown();
                     return;
                 }
+
                 CheckTime();
             }
         }
 
         private void ShutDown()
         {
-            // todo add some shut down mechanics
+            onBurnedEvent.ObjectBurned();
             GrowFire(-numOfHits);
             this.enabled = false;
             _numBurned++;
@@ -96,13 +119,22 @@ namespace Gilad
             _powerLevel = _powerLevel <= numOfHits ? _powerLevel : numOfHits;
             _powerLevel = _powerLevel >= 0 ? _powerLevel : 0;
             SetSizes();
-
         }
 
         private void SetSizes()
         {
+            var putOut = _powerLevel == 0;
+            
             for (int i = 0; i < flames.Length; i++)
             {
+                if (putOut)
+                {
+                    flames[i].SetActive(false);
+                }
+                else
+                {
+                    flames[i].SetActive(true);
+                }
                 flames[i].transform.localScale = _startSizes[i] * _powerLevel;
             }
         }
