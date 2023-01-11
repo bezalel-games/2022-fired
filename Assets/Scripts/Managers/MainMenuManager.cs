3fe6e7c052f9585a2443671caa66bdad948cef99
Bar Nemesh <barnemesh@gmail.com>
2023-01-11 17:46:09 +0200

    with better placements

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using BitStrap;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 using Logger = Nemesh.Logger;
 
@@ -9,8 +11,12 @@ namespace Managers
 {
     public class MainMenuManager : MonoBehaviour
     {
+        
         [SerializeField]
-        private int sceneToLoad = 1;
+        private int sceneToLoad = 1;  // TODO: scene selector drawer?
+
+        [SerializeField]
+        private LoadSceneMode loadMode = LoadSceneMode.Single;
         
         [ReadOnly]
         [SerializeField]
@@ -19,8 +25,25 @@ namespace Managers
 
         [ReadOnly]
         [SerializeField]
+        private float currentLoadingProgress;
+        
+        [ReadOnly]
+        [SerializeField]
         [InspectorName("Scene ready to load")]
         private bool sentReadyMassage;
+
+        [SerializeField]
+        public UnityEvent<float> onProgressBarUpdate;
+        
+        [SerializeField]
+        public UnityEvent onReadyState;
+        
+        [SerializeField]
+        public UnityEvent onStartSwitch;
+        
+        [SerializeField]
+        [InspectorName("On Load Complete - WARNING! USE WITH CAUTION")]
+        public UnityEvent onLoadComplete;
 
         private void Start()
         {
@@ -36,30 +59,41 @@ namespace Managers
         {
             yield return null;
 
-            AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(scene);
+            AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(scene, LoadSceneMode.Single);
             asyncOperation.allowSceneActivation = false;
             Logger.Log($"Start async load scene {scene}", this);
-
+            
             while (!asyncOperation.isDone)
             {
-
+                onProgressBarUpdate.Invoke(asyncOperation.progress);
+                currentLoadingProgress = asyncOperation.progress;
                 // Check if the load has finished
                 if (asyncOperation.progress >= 0.9f)
                 {
                     if (!sentReadyMassage)
                     {
                         Logger.Log($"Ready to switch to scene {scene}", this);
+                        onReadyState.Invoke();
                         sentReadyMassage = true;
                     }
                     if (loadNext)
                     {
                         Logger.Log($"Allowing scene {scene} activation");
+                        onStartSwitch.Invoke();
                         asyncOperation.allowSceneActivation = true;
                     }
                 }
 
                 yield return null;
             }
+
+            if (loadMode == LoadSceneMode.Additive)
+            {
+                Logger.Log("This is where you would unload current, for example.", this);
+                SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+            }
+            Logger.Log("Finished Load");
+            onLoadComplete.Invoke();
         }
         
     }
