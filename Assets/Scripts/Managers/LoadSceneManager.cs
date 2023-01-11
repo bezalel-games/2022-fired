bb98ffd73da3dc13d7727b0c3a7aa342df7517ba
Bar Nemesh <barnemesh@gmail.com>
2023-01-11 18:59:45 +0200

    Even better loading scene manager inspector

--- Assets/Scripts/Managers/LoadSceneManager.cs
+++ Assets/Scripts/Managers/LoadSceneManager.cs
@@ -10,14 +10,29 @@ using Logger = Nemesh.Logger;
 
 namespace Managers
 {
-    public class MainMenuManager : MonoBehaviour
+    public class LoadSceneManager : MonoBehaviour
     {
+
+        #region Inspector
+
+        [HelpBox(@"The following scene will be loaded.
+Notice you must enter the scene! Once loading started, changing this value is undefined.",
+            HelpBoxAttribute.MessageType.Info)]
+        [Header("Scene parameters")]
         [SerializeField]
-        private SceneReference sceneToLoad;  // TODO: scene selector drawer?
+        private SceneReference sceneToLoad;
 
         [SerializeField]
         private LoadSceneMode loadMode = LoadSceneMode.Single;
-        
+
+        [SerializeField]
+        [Tooltip("If additive load - should we unload this scene after?")]
+        private bool unloadIfAdditive = true;
+
+        [SerializeField]
+        private bool loadOnStart = true;
+
+        [Header("Progress Logs")]
         [ReadOnly]
         [SerializeField]
         [InspectorName("Received load next massage")]
@@ -26,26 +41,46 @@ namespace Managers
         [ReadOnly]
         [SerializeField]
         private float currentLoadingProgress;
-        
+
         [ReadOnly]
         [SerializeField]
         [InspectorName("Scene ready to load")]
         private bool sentReadyMassage;
 
+        [Header("Events for loading sequence")]
+        [Space]
         [SerializeField]
         public UnityEvent<float> onProgressBarUpdate;
-        
+
         [SerializeField]
         public UnityEvent onReadyState;
-        
+
+        [HelpBox("Notice - this may be just as the scene unloads, and some functions might not complete!",
+            HelpBoxAttribute.MessageType.Warning)]
         [SerializeField]
         public UnityEvent onStartSwitch;
-        
+
+        [HelpBox("if unloaded - this will never be reached!", HelpBoxAttribute.MessageType.Warning)]
         [SerializeField]
-        [InspectorName("On Load Complete - WARNING! USE WITH CAUTION")]
         public UnityEvent onLoadComplete;
 
+        #endregion
+
+        #region MonoBehaviour
+
         private void Start()
+        {
+            if (loadOnStart)
+            {
+                StartLoadScene();
+            }
+        }
+
+        #endregion
+
+        #region ScenLoader
+
+        public void StartLoadScene()
         {
             if (sceneToLoad.IsSafeToUse)
             {
@@ -56,6 +91,7 @@ namespace Managers
                 Logger.LogWarning("Not a valid scene!");
             }
         }
+
 
         public void GoToNext()
         {
@@ -69,7 +105,7 @@ namespace Managers
             AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(scene.BuildIndex, LoadSceneMode.Single);
             asyncOperation.allowSceneActivation = false;
             Logger.Log($"Start async load scene {scene.Name}", this);
-            
+
             while (!asyncOperation.isDone)
             {
                 onProgressBarUpdate.Invoke(asyncOperation.progress);
@@ -83,6 +119,7 @@ namespace Managers
                         onReadyState.Invoke();
                         sentReadyMassage = true;
                     }
+
                     if (loadNext)
                     {
                         Logger.Log($"Allowing scene {scene.Name} activation");
@@ -94,14 +131,17 @@ namespace Managers
                 yield return null;
             }
 
-            if (loadMode == LoadSceneMode.Additive)
+            if (loadMode == LoadSceneMode.Additive && unloadIfAdditive)
             {
                 Logger.Log("This is where you would unload current, for example.", this);
                 SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
             }
+
             Logger.Log("Finished Load");
             onLoadComplete.Invoke();
         }
-        
+
+        #endregion
+
     }
 }
